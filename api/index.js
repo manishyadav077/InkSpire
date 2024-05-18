3fed099a0f0c20af66d3b6e81bf6622a14068651
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2024-05-18 17:26:48 +0545

    added middleware to handle errors

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
-dotenv.config()
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URL)
@@ -17,11 +17,21 @@ mongoose
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running");
 });
 
-app.use('/api/user', userRoutes)
-app.use('/api/auth', authRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
