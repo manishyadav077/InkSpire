6c2022cddf1919fa8070a9e3e97e69f83bb4d9c4
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2024-05-23 21:58:11 +0545

    completed dashboard

---
+++ frontend/src/components/DashboardComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardComp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DashboardComp
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashComments from "../components/DashComments";
+import DashPosts from "../components/DashPosts";
+import DashProfile from "../components/DashProfile";
+import DashSidebar from "../components/DashSidebar";
+import DashUsers from "../components/DashUsers";
+import DashboardComp from "../components/DashboardComp";
 
 const Dashboard = () => {
-  return (
-    <div>
-      dashboard page
-    </div>
-  )
-}
+  const location = useLocation();
+  const [tab, setTab] = useState("");
 
-export default Dashboard
+  useEffect(() => {
+    const urlParams = new urlParams.get(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+      {tab === "profile" && <DashProfile />}
+
+      {tab === "posts" && <DashPosts />}
+
+      {tab === "users" && <DashUsers />}
+
+      {tab === "comments" && <DashComments />}
+
+      {tab == "dash" && <DashboardComp />}
+    </div>
+  );
+};
+
+export default Dashboard;
