1596af5cd1714b8d1173efd8a918b21aba939abd
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2024-05-26 19:49:24 +0545

    deployment

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
-// import path from 'path'
+import path from "path";
 
 dotenv.config();
 
@@ -19,7 +19,7 @@ mongoose
     console.log(err);
   });
 
-// const __dirname = path.resolve
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -35,7 +35,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-// app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get('*', (req, res) =>{
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
