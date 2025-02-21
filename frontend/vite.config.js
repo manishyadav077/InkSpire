280d334d2188921e92bf7346828ff87664fb44a3
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-02-21 12:50:32 +0545

    vite updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-    '/api':{
-      target: 'http://localhost:3000',
-      secure: false
-    }  
-    }
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 800,
   },
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(
+      process.env.NODE_ENV || "development"
+    ),
+  },
+});
