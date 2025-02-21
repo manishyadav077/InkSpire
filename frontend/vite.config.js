96403629b155323d34028d8d9b544c93cd08e78f
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-02-21 19:05:34 +0545

    quill prod bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,23 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ["quill", "quill-emoji"],
-  },
   server: {
     proxy: { "/api": { target: "http://localhost:3000", secure: false } },
   },
   build: {
     commonjsOptions: {
-      include: [/quill/, /node_modules/],
+      transformMixedEsModules: true, // Ensure compatibility with Quill (ESM + CJS)
     },
     chunkSizeWarningLimit: 800,
     rollupOptions: {
+      external: ["react"], // Prevent React from being bundled inside Quill
       output: {
+        globals: {
+          react: "React",
+        },
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            if (id.includes("quill")) return "quill";
-            if (id.includes("firebase")) return "firebase";
+            if (id.includes("firebase")) return "firebase"; // Keep Firebase separate
             return "vendor"; // General vendor chunk
           }
         },
