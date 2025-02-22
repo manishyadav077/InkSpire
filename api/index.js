b599cc960c95b15b8fff2df8e48406c3ae16dfd4
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-02-22 12:51:07 +0545

    cors issue fixed

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "https://ink-spire-puce.vercel.app",
+    origin: "https://ink-spire-theta.vercel.app",
     credentials: true,
   })
 );
