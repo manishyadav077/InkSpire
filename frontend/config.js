9c765ccb132a07872e3782384e9593279ac5593d
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-02-22 13:06:09 +0545

    base url updated

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,3 @@
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ||
-  "https://inkspire-production.up.railway.app";
+const API_BASE_URL = "https://inkspire-production.up.railway.app";
 
 export default API_BASE_URL;
