453e38924cfefb80ef9088f63d64422186d71b5b
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-02-22 12:36:52 +0545

    api base url issue fixed

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  "https://inkspire-production.up.railway.app";
 
 export default API_BASE_URL;
