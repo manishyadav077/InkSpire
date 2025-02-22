63857791cf510eb4429c61f1dd8f20436e726d2c
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-02-22 11:54:41 +0545

    cors error bug fix

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "https://inkspire0.netlify.app/",
+    origin: "https://ink-spire-puce.vercel.app/",
     credentials: true,
   })
 );
