5a91fa6afb9b94cf44e18c36bcf9f803eb4d63e8
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-11-07 11:18:17 +0545

    fix url issue

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,6 @@
-// const API_BASE_URL = "https://inkspire-production.up.railway.app";
-
-const API_BASE_URL = import.meta.env.VITE_API_URL;;
+const API_BASE_URL =
+  import.meta.env.MODE === "production"
+    ? ""
+    : import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export default API_BASE_URL;
-
-// "http://localhost:3000";
